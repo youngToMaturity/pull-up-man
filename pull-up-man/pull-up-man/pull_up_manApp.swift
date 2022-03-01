0d13641c3c84a5c3f6a2c5a359ef158966afc94c
loopbackseal <young961027@gmail.com>
2022-03-01 14:29:42 +0900

    Initial Commit

---
+++ pull-up-man/pull-up-man/pull_up_manApp.swift
@@ -0,0 +1,20 @@
+//
+//  pull_up_manApp.swift
+//  pull-up-man
+//
+//  Created by Young Soo Hwang on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct pull_up_manApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
