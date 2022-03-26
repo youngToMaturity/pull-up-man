743d067a33080056ef1a268263c49c3a4f817ba0
loopbackseal <young961027@gmail.com>
2022-03-26 18:37:53 +0900

    feat: Add nickname to test by uuid
    - test View에서 uuid를 통해 사용자의 nickname을 얻어와 표시하도록 했습니다.

--- pull-up-man/pull-up-man/View/MainView.swift
+++ pull-up-man/pull-up-man/View/MainView.swift
@@ -50,7 +50,9 @@ struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
             .environment(\.locale, .init(identifier: "ko"))
+            .environmentObject(UserViewModel("8880CD65-302C-4C57-AA26-421AEFC9456C"))
         MainView()
             .preferredColorScheme(.dark)
+            .environmentObject(UserViewModel("8880CD65-302C-4C57-AA26-421AEFC9456C"))
     }
 }
--- pull-up-man/pull-up-man/View/test.swift
+++ pull-up-man/pull-up-man/View/test.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct test: View {
+    @EnvironmentObject var userViewModel: UserViewModel
+    
     var body: some View {
-        Text("Coming Soon!")
+        VStack {
+            Text("Hello, \(userViewModel.nickname)!")
+                .font(.bold(.system(size: 28))())
+            Text("Comming Soon!")
+                .font(.subheadline)
+        }
     }
 }
 
 struct test_Previews: PreviewProvider {
     static var previews: some View {
         test()
+            .environmentObject(UserViewModel("8880CD65-302C-4C57-AA26-421AEFC9456C"))
     }
 }
